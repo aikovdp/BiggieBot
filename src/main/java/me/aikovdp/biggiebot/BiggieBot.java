31b74ad4caf0514042bf3e66b52ef3c4c6d17162
Aikovdp <aiko@aikovdp.me>
2021-11-16 13:52:44 +0100

    Initial commit

---
+++ src/main/java/me/aikovdp/biggiebot/BiggieBot.java
@@ -0,0 +1,46 @@
+package me.aikovdp.biggiebot;
+
+import me.aikovdp.biggiebot.modules.Starboard;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import org.jetbrains.annotations.NotNull;
+
+import javax.security.auth.login.LoginException;
+
+
+public class BiggieBot extends ListenerAdapter {
+
+    public static void main(String[] args) throws LoginException {
+        JDA jda = JDABuilder.createDefault(args[0])
+                .addEventListeners(new BiggieBot(), new Starboard())
+                .build();
+
+        jda.updateCommands().addCommands(
+                new CommandData("ping", "Ping the bot"),
+                new CommandData("poker", "Start a new poker session")
+                        .addOption(OptionType.CHANNEL, "channel", "The channel to start the session", true),
+                new CommandData("chess", "Start a new chess session")
+                        .addOption(OptionType.CHANNEL, "channel", "The channel to start the session", true)
+        ).queue();
+    }
+
+
+    @Override
+    public void onSlashCommand(@NotNull SlashCommandEvent event) {
+        switch (event.getName()) {
+            case "ping" -> event.reply("pong").queue();
+            case "poker" -> event.getOption("channel").getAsGuildChannel()
+                    .createInvite()
+                    .setTargetApplication("755827207812677713")
+                    .queue(invite -> event.reply(invite.getUrl()).queue());
+            case "chess" -> event.getOption("channel").getAsGuildChannel()
+                    .createInvite()
+                    .setTargetApplication("832012586023256104")
+                    .queue(invite -> event.reply(invite.getUrl()).queue());
+        }
+    }
+}
---
+++ src/main/java/me/aikovdp/biggiebot/modules/Starboard.java
@@ -0,0 +1,45 @@
+package me.aikovdp.biggiebot.modules;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.Button;
+
+import java.util.List;
+
+public class Starboard extends ListenerAdapter {
+    @Override
+    public void onMessageReactionAdd(MessageReactionAddEvent event) {
+        if (!event.isFromGuild()
+                || !event.getReactionEmote().getAsReactionCode().equals("\u2B50")
+                || event.getReaction().retrieveUsers().complete().size() != 1)
+            return;
+
+        List<TextChannel> starboardChannels = event.getGuild().getTextChannelsByName("starboard", false);
+        if (starboardChannels.isEmpty())
+            return;
+        TextChannel starboardChannel = starboardChannels.get(0);
+
+        Message message = event.retrieveMessage().complete();
+
+        MessageEmbed embed = new EmbedBuilder()
+                .setAuthor(message.getAuthor().getAsTag(), null, message.getAuthor().getEffectiveAvatarUrl())
+                .setDescription(message.getContentRaw())
+                .setColor(0xFFAE30)
+                .setTimestamp(message.getTimeCreated())
+                .build();
+
+        Message response = new MessageBuilder()
+                .setContent("\u2B50 " + event.getTextChannel().getAsMention())
+                .setActionRows(ActionRow.of(Button.link(message.getJumpUrl(), "Jump to message")))
+                .setEmbeds(embed)
+                .build();
+
+        starboardChannel.sendMessage(response).queue();
+    }
+}
