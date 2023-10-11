c3fddec9569c6d901e974c15b3efb38152d974b7
Aiko Vandeputte <aiko@aikovdp.me>
2023-10-11 18:11:36 +0200

    Migrate build scripts to Kotlin DSL and upgrade Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    application
+}
+
+application.mainClass = "me.aikovdp.biggiebot.BiggieBot"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+
+repositories {
+    mavenCentral()
+    maven (url = "https://m2.dv8tion.net/releases")
+}
+
+dependencies {
+    implementation("net.dv8tion:JDA:5.0.0-alpha.11")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
+}
